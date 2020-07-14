08c9ded22f806df59e499bff8279f9d7d07ccd53
Ericsii <30063838+Ericsii@users.noreply.github.com>
2020-07-14 22:33:27 +0800

    Add Text

--- include/components/text.h
+++ include/components/text.h
@@ -1,10 +1,31 @@
 /*
- * @Author:
+ * @Author: Eric Feng
  * @Date: 2020-07-13 16:19:53
- * @LastEditTime: 2020-07-14 17:23:41
+ * @LastEditTime: 2020-07-14 22:17:02
  * @LastEditors: Please set LastEditors
- * @Description: 
+ * @Description: Text outer.
  * @FilePath: \landlord-ai\include\components\text.h
  */
 
 #pragma once
+
+#include <string>
+#include "graphics.h"
+
+
+class Text {
+public:
+    Text(LPCSTR text_str = "", Rect rect_pos = Rect(), LPCSTR font_face = "黑体");
+    ~Text();
+
+    void SetText(LPCSTR text_str);
+    void SetPos(Rect rect_pos);
+    void PutText();
+
+private:
+    void _Destroy();
+
+    LPCSTR out_text_;
+    LPCSTR font_face_;
+    Rect rect_pos_;
+};
