388f1f5f66fda16fe5ed3c3a8131dfe006f36650
magicgh <magicgher@hotmail.com>
2020-07-16 11:37:02 +0800

    :sparkles: add some feature to game.h

--- include/game.h
+++ include/game.h
@@ -33,10 +33,11 @@ private:
     ExplainScene explain_scene;
     Scene *current_scene;
     Player *player[3], *landlord, *current_player, *last_player;
-    Cards card_heap_;
-    int landlord_cards[3];
-    Status stage_;
-    int times,questioned;//倍率,叫地主次数
+    Cards card_heap_;//发牌堆
+    int landlord_cards[3];//地主专属牌
+    int call_score[3];//各家叫地主的分数
+    Status stage_;//游戏进度
+    int times,questioned, base_score, first_call;//倍率,询问叫地主次数,本局基本分, 第一个叫地主的玩家
 
 private:
     void init();
@@ -45,12 +46,14 @@ public:
     Game();
     ~Game();
     void main();
+    //当前玩家的下家在玩家指针数组中的下标
     inline int nextPlayerIndex(){
         for(int i = 0; i < 3; i++)
             if(player[i] == current_player)
                 return (i+1)%3;
         return -1;
     }
+    //获取当前玩家的下家
     inline Player* nextPlayer(){
         return player[nextPlayerIndex()];
     }
@@ -61,6 +64,7 @@ public:
                 return (i+2)%3;
         return -1;
     }
+    //获取当前玩家的上家
     inline Player* prevPlayer(){
         return player[prevPlayerIndex()];
     }
@@ -68,15 +72,22 @@ public:
     inline bool isHumanTurn(){
         return current_player == player[0];
     }
-
+    //获取游戏的当前进度
     inline Status getStatus() {
         return stage_;
     }
-
-    void sendCard();
-    void getLandlord();
-
+    inline void switchScene(Scene *s){
+        current_scene = s;
+    }
+    void sendCard();//发牌
+    void getLandlord(); //叫地主
+    void sendLandlordCard(); //发地主牌
+    void pass(); //过牌
+    void gameOver(); //游戏结束
+    void hint(); //出牌提示
     void discard();
+    void sendScore(int result); //设置真人玩家叫地主的分数
+
 
 
 };
